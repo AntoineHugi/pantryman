1d3a49ab5d199ed13939f5a05d724c41a406900b
Yi Lei Jiang <yi.lei.jiang@wolt.com>
2025-06-09 15:59:09 +0200

    Add Ktor

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     kotlin("jvm") version "2.1.20"
+    application
+    id("io.ktor.plugin") version "3.1.3"
 }
 
 group = "org.pantry"
@@ -9,7 +11,20 @@ repositories {
     mavenCentral()
 }
 
+application {
+    // mainClass = "org.pantry.MainKt"
+    mainClass = "io.ktor.server.netty.EngineMain"
+}
+
 dependencies {
+    implementation("io.ktor:ktor-server-core-jvm")
+    implementation("io.ktor:ktor-server-netty")
+    implementation("ch.qos.logback:logback-classic:1.4.11")
+    implementation("io.ktor:ktor-server-core")
+    implementation("io.ktor:ktor-server-config-yaml")
+
+    testImplementation("io.ktor:ktor-server-test-host")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
     testImplementation(kotlin("test"))
 }
 
