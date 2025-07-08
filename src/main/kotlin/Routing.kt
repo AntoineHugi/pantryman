30092a5a58f6c525fe2e07c0a597c5bc2f174dd1
AntoineHugi <antoine.hugi@gmail.com>
2025-07-08 19:38:13 +0200

    update gitignore

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -4,10 +4,62 @@ import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
-fun Application.configureRouting() {
+fun Application.menuApi() {
     routing {
-        get("/") {
+        get(
+            "/menu") {
+            call.respondText("Hello World!")
+        }
+        get("/userSettings") {
+            call.respondText("Hello World!")
+        }
+        get("/appSettings") {
             call.respondText("Hello World!")
         }
     }
 }
+
+fun Application.groceriesApi() {
+    routing {
+        route("/lists") {
+            post {
+                call.respondText("Create a new grocery list")
+            }
+            get {
+                call.respondText("Display all grocery lists")
+            }
+            route("/{listId}") {
+                get {
+                    call.respondText("Display specific list by ID")
+                }
+                put {
+                    call.respondText("Rename the list")
+                }
+                delete {
+                    call.respondText("Delete the list")
+                }
+                post("/duplicate") {
+                    call.respondText("Duplicate the list")
+                }
+                route("/items") {
+                    post {
+                        call.respondText("Add item to list")
+                    }
+                    route("/{itemId}") {
+                        delete {
+                            call.respondText("Delete item from list")
+                        }
+                        post("/increase") {
+                            call.respondText("Increase item quantity")
+                        }
+                        post("/decrease") {
+                            call.respondText("Decrease item quantity")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
