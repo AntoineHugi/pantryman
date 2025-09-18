3ebff6fa42dda285e6b6a8be6944f11e4448cdb4
AntoineHugi <antoine.hugi@gmail.com>
2025-09-18 11:16:24 +0200

    Added unit tests and integration tests

--- src/main/kotlin/org/pantry/di/AppModule.kt
+++ src/main/kotlin/org/pantry/di/AppModule.kt
@@ -8,8 +8,8 @@ import org.pantry.services.GroceryListService
 import org.pantry.services.ItemService
 
 val appModule = module {
-    single { GroceryListRepository() }
-    single { GroceryListService(get()) }
     single { ItemRepository() }
     single { ItemService(get()) }
+    single { GroceryListRepository( ItemRepository() ) }
+    single { GroceryListService(get()) }
 }
