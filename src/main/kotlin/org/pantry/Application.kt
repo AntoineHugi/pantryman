2f9e31ba6f567c77da4af3d369b413b568aded4f
AntoineHugi <antoine.hugi@gmail.com>
2025-10-14 15:16:30 +0200

    updated application.yaml

--- src/main/kotlin/org/pantry/Application.kt
+++ src/main/kotlin/org/pantry/Application.kt
@@ -2,7 +2,6 @@ package org.pantry
 
 import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.*
-import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.plugins.cors.routing.*
 import io.ktor.http.*
